2c88313e9a2a40866ffde98c2f69e9633f4efbb7
uvraaj <yuvrajguleria45@gmail.com>
2025-11-11 15:42:32 +0530

    feat:

---
+++ src/com/yuvraj/DSA/Linked_lists/DLL/Main.java
@@ -0,0 +1,46 @@
+package com.yuvraj.DSA.Linked_lists.DLL;
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    Node (int data, Node prev, Node next) {
+        this.data = data;
+        this.prev = prev;
+        this.next = next;
+    }
+    Node (int data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+        }
+
+public  class Main{
+    public static void main(String[] args) {
+            int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            Node head = convert_arr2DLL(arr);
+            print_DLL(head);
+    }
+    private static Node convert_arr2DLL(int[] arr) {
+        Node head = new Node(arr[0]);
+        Node prev = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i], prev, null);
+            prev.next = temp;
+            prev = temp;
+        }
+        return head;
+    }
+    private static void print_DLL(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+    }
+}
