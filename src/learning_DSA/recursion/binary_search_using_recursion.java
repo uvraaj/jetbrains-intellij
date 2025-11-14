16791ba514070fe06c7799281b8ac3b5eb282e82
uvraaj <yuvrajguleria45@gmail.com>
2025-11-14 22:54:44 +0530

    master - Implement binary search algorithm using recursion

--- src/learning_DSA/recursion/binary_search_using_recursion.java
+++ src/learning_DSA/recursion/binary_search_using_recursion.java
@@ -1,4 +1,20 @@
 package learning_DSA.recursion;
 
 public class binary_search_using_recursion {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int target = 9;
+        System.out.println("target value " + target + " is at index " + search(arr, target, 0, arr.length-1));
+    }
+    static int search(int[] arr, int target, int start, int end) {
+        if (start > end) { return -1; }
+
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target) { return mid; }
+        if (arr[mid] > target) { return search(arr, target, start, mid-1); }
+        if (arr[mid] < target) { return search(arr, target, mid+1, end); }
+
+        return -1;
+    }
 }
