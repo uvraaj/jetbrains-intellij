8a9acfafd3349568ca55337f887a413741cdbce9
uvraaj <yuvrajguleria45@gmail.com>
2025-11-15 02:18:13 +0530

    Fix base case in digit sum recursion to handle single-digit numbers

--- src/learning_DSA/recursion/kunal_kushwaha/Level_1/digitSum.java
+++ src/learning_DSA/recursion/kunal_kushwaha/Level_1/digitSum.java
@@ -2,7 +2,7 @@ package learning_DSA.recursion.kunal_kushwaha.Level_1;
 
 public class digitSum {
     static int sum(int n) {
-        if (n == 0) { return 0; }
+        if (n%10 == n) { return n; }
 
         return (n%10) + sum(n/10);
     }
