5a77ec14e3b4bd707c564bbf195884c7a74c6c07
uvraaj <yuvrajguleria45@gmail.com>
2025-11-15 00:52:49 +0530

    master - Implement factorial calculation using recursion

---
+++ src/learning_DSA/recursion/kunal_kushwaha/Level_1/factorial.java
@@ -0,0 +1,19 @@
+package learning_DSA.recursion.kunal_kushwaha.Level_1;
+
+import java.util.Scanner;
+
+public class factorial {
+    static long fact(long n) {
+
+        if (n == 1) { return 1; }
+
+        return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        long n = in.nextLong();
+        System.out.println("factorial of " + n + " is : " +  fact(n));
+    }
+}
