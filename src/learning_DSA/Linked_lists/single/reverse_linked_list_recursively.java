512cf302f0a5b4226d4f95d3fb4dc20978619eeb
uvraaj <yuvrajguleria45@gmail.com>
2025-11-28 11:49:59 +0530

    Add recursive method to reverse a linked list and print it

--- src/learning_DSA/Linked_lists/single/reverse_linked_list_recursively.java
+++ src/learning_DSA/Linked_lists/single/reverse_linked_list_recursively.java
@@ -2,4 +2,44 @@ package learning_DSA.Linked_lists.single;
 
 public class reverse_linked_list_recursively {
 
+    public static Node reverseLinkedListRecursive(Node head) {
+        // 1. Base case: If head is empty or reached end
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // 2. Recursive Hypothesis: Reverse the rest of the list
+        Node newHead = reverseLinkedListRecursive(head.next);
+
+        // 3. Induction: Change the pointers
+        Node front = head.next; // 'front' is the node ahead of current
+        front.next = head;      // Make 'front' point back to 'head'
+        head.next = null;       // Break the link from 'head' to 'front'
+
+        return newHead;         // Return the new head (which was the original tail)
+    }
+
+    public static void printLinkedList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        System.out.println("Original linked list:");
+        printLinkedList(head);
+
+        System.out.println("Reversed linked list (Recursive):");
+        Node newhead = reverseLinkedListRecursive(head);
+        printLinkedList(newhead);
+    }
 }
