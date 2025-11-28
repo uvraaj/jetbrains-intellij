ccbaf2e4ebc609692ea6d56931975e7cd6dceb94
uvraaj <yuvrajguleria45@gmail.com>
2025-11-28 12:03:58 +0530

    Add method to print linked list in a formatted manner

--- src/learning_DSA/Linked_lists/single/Node.java
+++ src/learning_DSA/Linked_lists/single/Node.java
@@ -12,4 +12,12 @@ public class Node {
         this.data = data;
         this.next = null;
     }
+    public static void printLinkedList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
 }
