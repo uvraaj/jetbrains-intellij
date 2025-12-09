ee1d87f98a1ca7438ae6f15870d5c9e537ef46c6
uvraaj <yuvrajguleria45@gmail.com>
2025-12-09 11:14:47 +0530

    Add method to find all indices of a target in an array using recursion

---
+++ src/learning_DSA/recursion/kunal_kushwaha/Subset_Subsequence_String_Questions/remove_character.java
@@ -0,0 +1,26 @@
+package learning_DSA.recursion.kunal_kushwaha.Subset_Subsequence_String_Questions;
+
+public class remove_character {
+    public static void main(String[] args) {
+        String str = "bcaccda";
+        char character_to_remove = 'a';
+        int starting_index = 0;
+        System.out.println(remove_CHARACTER(str, character_to_remove, starting_index));
+//        System.out.println(remove_CHAR_ALT(str, character_to_remove));
+    }
+
+    private static String remove_CHARACTER(String str, char character_to_remove, int current_index) {
+
+        //base case
+        if(current_index == str.length()) {
+            return "";
+        }
+
+        char current_character = str.charAt(current_index);
+        String result_of_rest = remove_CHARACTER(str, character_to_remove, current_index + 1);
+
+        if (current_character == character_to_remove) { return result_of_rest; }
+        else { return current_character + result_of_rest; }
+    }
+//    private static String remove_CHAR_ALT(String str, char character_to_remove){}
+}
